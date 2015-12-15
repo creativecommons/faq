3ce823829009fbde37da7f347824efde35d7437b
Matt Lee <mattl@cnuk.org>
2015-12-15 12:54:16 -0600

    Updates

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 pandoc french-faq.md -o faq-fr.html --toc --toc-depth=3 --template template.html -V "pagetitle: Foire Aux Questions" -V "title: Foire Aux Questions"
 pandoc faq.md -o faq-en.html --toc --toc-depth=3 --template template.html -V "pagetitle: Frequently Asked Questions" -V "title: Frequently Asked Questions"
 
+git add .
+git commit -m "Updates"
+rsync *.html mattl@labs.creativecommons.org:~/public_html/
+git push origin master
