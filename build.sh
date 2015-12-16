2ac980303045370801c3f0ce69cc81d1c99afb58
Matt Lee <mattl@cnuk.org>
2015-12-15 20:23:09 -0600

    Updates

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-pandoc french-faq.md -o faq-fr.html --toc --toc-depth=3 --number-sections --from markdown+implicit_header_references -html5 --template template.html -V "pagetitle: Foire Aux Questions" -V "title: Foire Aux Questions"
-pandoc faq.md -o faq-en.html --toc --toc-depth=3 --template template.html -V "pagetitle: Frequently Asked Questions" -V "title: Frequently Asked Questions"
+pandoc french-faq.md -o faq-fr.html --toc --toc-depth=3 --number-sections --from markdown+implicit_header_references -w html5 --template template.html -V "pagetitle: Foire Aux Questions" -V "title: Foire Aux Questions"
+pandoc faq.md -o faq-en.html --toc --toc-depth=3 --number-sections --from markdown+implicit_header_references -w html5 --template template.html -V "pagetitle: Frequently Asked Questions" -V "title: Frequently Asked Questions"
 
 git add .
 git commit -m "Updates"
