4a8ef0a817e0beca87eff10c375819b8601a5280
Matt Lee <mattl@cnuk.org>
2015-12-16 20:21:14 -0600

    Updates

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-pandoc faq-fr.md -o faq-fr.html --toc --toc-depth=4 -w html5 --template template.html -V "pagetitle: Foire Aux Questions" -V "title: Foire Aux Questions" -V "lastmodified: $(date)"
+pandoc faq-fr.md -o faq-fr.html --toc --toc-depth=4 -w html5 --template template.html -V "pagetitle: Foire Aux Questions" -V "title: Foire Aux Questions" -V "lastmodified: $(date)" -V toctitle:"Table des matières"
 pandoc faq-en.md -o faq-en.html --toc --toc-depth=4 -w html5 --template template.html -V "pagetitle: Frequently Asked Questions" -V "title: Frequently Asked Questions" -V "lastmodified: $(date)"
 
 git add .
