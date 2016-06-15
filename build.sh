117e4e280d58afc8b83dcf9c0d1a757b7daf6dbb
Matt Lee <mattl@users.noreply.github.com>
2016-06-15 16:41:51 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 pandoc faq-fr.md -o faq-fr.html --toc --toc-depth=4 -w html5 --template template.html -V "pagetitle: Foire Aux Questions" -V "title: Foire Aux Questions" -V "lastmodified: $(date)" -V toctitle:"Table des matières"
 pandoc faq-en.md -o faq-en.html --toc --toc-depth=4 -w html5 --template template.html -V "pagetitle: Frequently Asked Questions" -V "title: Frequently Asked Questions" -V "lastmodified: $(date)"
 
-git add .
-git commit -m "Updates"
-rsync --progress *.html mattl@labs.creativecommons.org:~/public_html/
-git push origin master
