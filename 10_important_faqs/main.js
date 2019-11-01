05a6841aa2b252771bec94069080dbe8eb0f7c73
GettyOrawo <gettyorawo@gmail.com>
2019-11-02 02:18:40 +0300

    Add search bar feature

--- 10_important_faqs/main.js
+++ 10_important_faqs/main.js
@@ -15,6 +15,25 @@ jQuery(document).ready(function($) {
     dropDown.stop(false, true).slideToggle();
 
     j.preventDefault();
+  });
+
+  $('.live-search-list li').each(function(){
+  $(this).attr('data-search-term', $(this).text().toLowerCase());
+  });
+
+  $('.live-search-box').on('keyup', function(){
+
+    var searchTerm = $(this).val().toLowerCase();
+
+    $('.live-search-list li').each(function(){
+
+      if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
+          $(this).show();
+      } else {
+          $(this).hide();
+      }
+
+    });
 
   });
 });
